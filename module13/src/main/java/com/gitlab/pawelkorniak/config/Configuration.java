09817cd3960c807469f9a1daca334a49282ae2a3
p.korniak <p.korniak@eigroup.com>
2022-11-02 10:41:36 +0100

    storage depends on props

--- module13/src/main/java/com/gitlab/pawelkorniak/config/Configuration.java
+++ module13/src/main/java/com/gitlab/pawelkorniak/config/Configuration.java
@@ -2,9 +2,12 @@ package com.gitlab.pawelkorniak.config;
 
 import com.gitlab.pawelkorniak.facade.BookingFacadeImpl;
 import com.gitlab.pawelkorniak.model.Event;
+import com.gitlab.pawelkorniak.model.Ticket;
+import com.gitlab.pawelkorniak.model.User;
 import com.gitlab.pawelkorniak.service.EventService;
 import com.gitlab.pawelkorniak.service.TicketService;
 import com.gitlab.pawelkorniak.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 import java.util.HashMap;
@@ -13,9 +16,22 @@ import java.util.Map;
 @org.springframework.context.annotation.Configuration
 public class Configuration {
 
+    @Value("memoryStorage")
+    boolean isMemoryStorageSet;
     @Bean
-    public Map<Long, Event> storage(){
-        return new HashMap<>();
+    public Map<Long, Event> eventStorage(){
+
+        return isMemoryStorageSet ? new HashMap<>() : null;
+    }
+
+    @Bean
+    public Map<Long, Ticket> ticketStorage(){
+        return isMemoryStorageSet ? new HashMap<>() : null;
+    }
+
+    @Bean
+    public Map<Long, User> userStorage(){
+        return isMemoryStorageSet ? new HashMap<>() : null;
     }
 
     @Bean
