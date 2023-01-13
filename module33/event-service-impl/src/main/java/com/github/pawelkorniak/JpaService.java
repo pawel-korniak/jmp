9e64ec75bef175ef17034e12ad88cfbe7999c40b
p.korniak <p.korniak@eigroup.com>
2023-01-13 10:24:36 +0100

    swagger-ui works

--- module33/event-service-impl/src/main/java/com/github/pawelkorniak/JpaService.java
+++
@@ -1,8 +0,0 @@
-package com.github.pawelkorniak;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface JpaService extends JpaRepository<Event,Long> {
-}
