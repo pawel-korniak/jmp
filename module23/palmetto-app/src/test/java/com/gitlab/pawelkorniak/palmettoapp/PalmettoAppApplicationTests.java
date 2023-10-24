951ea6fb697a4c966ef4a27a1c2c4ee0e5a2d45a
pawel <pawello1020@gmail.com>
2023-10-24 18:03:14 +0200

    check

---
+++ module23/palmetto-app/src/test/java/com/gitlab/pawelkorniak/palmettoapp/PalmettoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitlab.pawelkorniak.palmettoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalmettoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
